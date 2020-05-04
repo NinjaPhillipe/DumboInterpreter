8a77f7b149574d7888624849fd7cf30e8add1b33
NinjaPhillipe <23127972+NinjaPhillipe@users.noreply.github.com>
2020-05-04 17:09:23 +0000

    End

--- dumbo_interpreter.py
+++ dumbo_interpreter.py
@@ -1,49 +1,58 @@
 from lark import Lark
 from lark import Transformer
-import logging
-logging.basicConfig(level=logging.DEBUG)
+import sys
 
-# print(dumbo_grammar)
-
-# lol on sait pas si il faut un espace apres for
-
+# Variable global
 dic= {}
 
 def getStringInterior(tree,tmp):
-    print(" add "+tree.children[0].children[0].value)
+    """
+        Return a string that represents the string interior
+        
+        Recursive algorithm
+    """
     tmp.append(tree.children[0].children[0].value)
     if(len(tree.children)==2):
         getStringInterior(tree.children[1],tmp)
     return tmp
 
 def getVar(tree):
-    # print("______GET VAR  "+str(tree))
+    """
+        Use the getValue,getString and evalInteger to get the value assigned to the variable 
+        Recursive function that go further in the tree everytime it's called
+    """
     if(tree.data == "string_expression"):
         if(tree.children[0].data == "string"):
             return tree.children[0].children[0]
         elif(tree.children[0].data == "variable"):
             return getValue(tree.children[0].children[0])
         elif(tree.children[0].data == "string_expression"):
+            # if the child is a string expression apply getVar again on the child
             if(len(tree.children)== 2):
                 return getVar(tree.children[0])+getVar(tree.children[1])
             return getVar(tree.children[0])
     elif(tree.data == "integer"):
-
-        print(tree.children)
-        return (tree.children[0])
-        
-        
+        return evalInteger(tree)        
         
     elif(tree.data == "string_list"):
         return getStringInterior(tree.children[0],[])
     return "ERROR"
 
-# def getValue(tree):
-#     return dic[tree.children[0]]
 def getValue(variable):
-    return dic[variable]
+    """
+        Return the value of a key if the key exist.
+    """
+    if(dic.__contains__(variable)):
+        return dic[variable]
+    else:
+        print("Variable : "+str(variable) + " ERROR KEY NOT IN DIC")
 
 def executeFor(tree,file):
+    """
+    Function called when there's a 'for' encounter in the tree
+    A tmp variable is created so the 'true' value of the variable
+    in the list is not modified
+    """
     if(tree.data == "for" ):   
         # tokken where it symbol is stocked  
         tokkenIT = tree.children[0].children[0]
@@ -53,16 +62,16 @@ def executeFor(tree,file):
         if(dic.__contains__(tokkenIT)):
             tmp = dic.pop(tokkenIT)
 
+        # get the value on which the iteration is executed
         if(tree.children[1].data == "variable" ):
             ot = getValue(tree.children[1].children[0])
         elif (tree.children[1].data == "string_list" ):
-            print("pipi -------")
             ot = getStringInterior(tree.children[1].children[0],[])
-            print("pipi ------- "+ str(ot))
-            # print("STRING LIST FOR NOT YET IMPLEMENTED")
         
+        # compute the iteration
         for it in ot:
             dic[tokkenIT] = it
+            # sanity check
             if(tree.children[2].data == "expression_list" ):
                 explore(tree.children[2],file )
         
@@ -75,17 +84,30 @@ def executeFor(tree,file):
         print("Incorrect for ")
 
 def evalInteger(tree):
-    if(len(tree.children) == 1):
+    """
+    Fuction used when a definition of integer is encountered in the tree
+    in order to compute the value of an integer 
+    
+    if we are in a leaf of the tree, the function is used to return the value of the leaf
+    if we are not in a leaf the function is called on the subtree to calculate the value of the subtree
+    """
+    # if the subtree is a leaf
+    if(tree.children[0].data == "int"):
         return int(tree.children[0].children[0].value)
     
+    # set Var 1
     if(tree.children[0].data == "integer"):
         var1 = evalInteger(tree.children[0])
+    if(tree.children[0].data == "variable"):
+        var1 = getValue(tree.children[0].children[0].value)
 
+    # set Var 2 
     if(tree.children[2].data == "integer"):
         var2 = evalInteger(tree.children[2])
+    if(tree.children[2].data == "variable"):
+        var2 = getValue(tree.children[2].children[0].value)
 
-    # faire cas variable
-
+    # Operation
     if(tree.children[1].children[0].data == "mult"):
         return var1 * var2
     elif(tree.children[1].children[0].data == "plus"):
@@ -95,32 +117,34 @@ def evalInteger(tree):
     elif(tree.children[1].children[0].data == "minus"):
         return var1 - var2
 
-    print("INTERDIT")
+    print("ERROR : UNEXPECTED TOKKEN")
 
 def evalBoolean(tree):
+    """
+        Function that evaluate the boolean.
+
+        If the boolean children is a "or" or "and" tokken 
+        recursive call are executed on his two member.
+    """
+    # check if children the children is a "or" or a "and" tokken
     if (tree.children[0].data == "or"):
         return evalBoolean(tree.children[0].children[0]) or evalBoolean(tree.children[0].children[1])
-
     if (tree.children[0].data) == "and":
         return evalBoolean(tree.children[0].children[0]) and evalBoolean(tree.children[0].children[1])
+    
+    # set var1
     if(tree.children[0].data == "integer"):
         var1 = evalInteger(tree.children[0])
     elif(tree.children[0].data == "variable"):
-        print(getValue(tree.children[0].children[0]))
         var1 = getValue(tree.children[0].children[0].value)
 
-
+    # set var2
     if(tree.children[2].data == "integer"):
         var2 = evalInteger(tree.children[2])
     elif(tree.children[2].data == "variable"):
-        print(getValue(tree.children[2].children[0]))
         var2 = getValue(tree.children[2].children[0].value)
 
-    
-
-
     if(tree.children[1].children[0].data == "greater"):
-
         return var1 > var2
     if(tree.children[1].children[0].data == "less"):
         return var1 < var2
@@ -129,17 +153,21 @@ def evalBoolean(tree):
     if(tree.children[1].children[0].data == "nequal"):
         return var1 != var2
 
-    print("NE DOIT PAS ETRE LA ")
+    print("ERROR : UNEXPECTED TOKKEN")
     return False
 
 def executeIf(tree,file):
+    """
+        Compute expression list if the evalution is true
+    """
     if(evalBoolean(tree.children[0])):
         explore(tree.children[1],file)
 
 
 def explore(racine,file_out):
-    print(racine.data)
-    # traite le cas de base
+    """
+        Explore recursively the tree to compute his tokken. 
+    """
     if racine.data == "programme":
         if(type(racine.children)==list):
             for el in racine.children:
@@ -161,7 +189,7 @@ def explore(racine,file_out):
     elif racine.data == "expression":
         if(racine.children[0].data == "print"):
             if file_out is not None:
-                file_out.write(getVar(racine.children[0].children[0]) )
+                file_out.write(str(getVar(racine.children[0].children[0])) )
                 
         elif(racine.children[0].data == "if"):
             executeIf(racine.children[0],file_out)
@@ -170,51 +198,22 @@ def explore(racine,file_out):
         elif(racine.children[0].data == "variable"):
             dic[racine.children[0].children[0]] = getVar(racine.children[1])
 
-    elif racine.data == "variable":
-        print("VARIABLE")
-        print(racine.children[0])
-        print(racine.children)
-        print("")
-
-
 if(__name__=="__main__"):
+    if(len(sys.argv) == 4 ):
+        with open( "dumboGrammar.lark", "r") as grammar:
+            parserDumbo =  Lark(grammar.read(),start="programme")
+        
+        with open( sys.argv[1], "r" ) as texte:
+            tree_data = parserDumbo.parse(texte.read())
 
-    with open( "dumboGrammar.lark", "r") as grammar:
-        parserDumbo =  Lark(grammar.read(),start="programme")
+        with open( sys.argv[2], "r" ) as texte:
+            tree_template = parserDumbo.parse(texte.read())
 
+        # load data from data file
+        explore(tree_data,None)
 
-    with open( "data1.dumbo", "r" ) as texte:
-        tree_data = parserDumbo.parse(texte.read())
-        # print(data.pretty())
-
-
-    with open( "template.dumbo", "r" ) as texte:
-        tree_template = parserDumbo.parse(texte.read())
-        # print(template.pretty())
-
-
-    # print(tree_data)
-    # print(tree_data.data)
-    # print(tree_data.children)
-    # print(tree_data.children[0])
-    # print(tree_data.children[0].data.children)
-
-    # print(tree_template)
-
-    #objet tree
-
-
-    #load data
-    explore(tree_data,None)
-    #execute template
-    with open( "output.html", "w" ) as output:
-        explore(tree_template,output)
-
-
-    print("nom = "+str(dic["nom"]))
-    print("prenom = "+str(dic["prenom"]))
-    print("cours = "+str(dic["cours"]))
-    print("lol = "+str(dic["lol"]))
-    print("mdr = "+str(dic["mdr"]))
-
-    # Tree(op, [Tree(plus, [])])
+        #execute template
+        with open( sys.argv[3], "w" ) as output:
+            explore(tree_template,output)
+    else:
+        print("Incorrect argument format")
